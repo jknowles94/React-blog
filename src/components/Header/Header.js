0ee96c4e07f95522c674327a7eb0548506aedd9a
Jake Knowles <jakeyknowles@hotmail.co.uk>
2019-03-06 11:38:55 +0000

    add burger nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,8 +11,8 @@ const Header = (props) => {
         <Container>
           <Row>
             <Col className="py-3">{props.children}</Col>
-            <Col sm={8}>
-              <Nav activeKey="/">
+            <Col className="py-3 py-sm-0" xs={2} sm={8}>
+              <Nav activeKey="/" className="d-none d-sm-flex">
                 <Nav.Item>
                   <Nav.Link className="py-3" href="/">Home</Nav.Link>
                 </Nav.Item>
@@ -20,6 +20,9 @@ const Header = (props) => {
                   <Nav.Link className="py-3">Create New Blog</Nav.Link>
                 </Nav.Item>
               </Nav>
+              <div className="d-block d-sm-none">
+                <div className="BurgerNav"><span></span><span></span><span></span></div>
+              </div>
             </Col>
           </Row>
         </Container>
