7f857da48a989bccaf049c0d872ea8da60c721c6
Jake Knowles <jakeyknowles@hotmail.co.uk>
2019-03-20 17:09:51 +0000

    new ui components for buttons and inputs

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Newsletter.scss';
+import Button from '../UI/Button/Button';
 
 const Newsletter = (props) => {
     return (
       <div className="NewsletterSignUp">
         <h1>Newsletter</h1>
         <p>Donec elementum dui semper, pretium dui quis, pretium nisl.</p>
+        <Button>Test Button</Button>
       </div>
     );
 }
---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = (props) => {
+  return (
+    <p>{props.children}</p>
+  );
+}
+
+export default Button;
