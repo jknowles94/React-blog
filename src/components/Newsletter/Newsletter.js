0cf6e6f5088ded1806e0b4f712608e606068a0ce
Jake Knowles <jakeyknowles@hotmail.co.uk>
2019-03-21 16:10:04 +0000

    add basic input

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Newsletter.scss';
 import Button from '../UI/Button/Button';
+import Input from '../UI/Input/Input';
 
 const Newsletter = (props) => {
     return (
       <div className="NewsletterSignUp">
         <h1>Newsletter</h1>
         <p>Donec elementum dui semper, pretium dui quis, pretium nisl.</p>
+        <Input/>
         <Button>Test Button</Button>
       </div>
     );
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './Input.scss';
 
 const Input = (props) => {
-    return ();
+  let input = null;
+  // switch(props.inputType){
+  //   case('input') {
+
+  //   }
+  // }
+  return (
+    <div>
+      {input}
+    </div>
+  );
 }
 
 export default Input;
